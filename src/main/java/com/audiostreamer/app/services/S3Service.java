7c09fb762a75dd87e565c434fa4016ac1a938c67
Jibriil Guure <jabriilguure@gmail.com>
2025-04-26 16:14:44 +0800

    simple s3 servise added

---
+++ src/main/java/com/audiostreamer/app/services/S3Service.java
@@ -0,0 +1,33 @@
+package com.audiostreamer.app.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+
+@Service
+public class S3Service {
+
+    @Value("${aws.s3.bucket}")
+    private String bucketName;
+
+    private final S3Client s3Client;
+
+    public S3Service(S3Client s3Client) {
+        this.s3Client = s3Client;
+    }
+
+    public void uploadFile(String key, MultipartFile file) throws IOException {
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .contentType(file.getContentType())
+                .build();
+
+        s3Client.putObject(request, RequestBody.fromBytes(file.getBytes()));
+    }
+}
